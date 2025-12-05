5e90524d72a3f298caf01a98e139a88ace38c1a9
cstuart756 <stujjc@yahoo.co.uk>
2025-12-05 09:44:01 +0000

    add apps/products/models.py

---
+++ apps/products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_in_stock(self):
+        return self.stock > 0
+
+    def __str__(self):
+        return self.name
