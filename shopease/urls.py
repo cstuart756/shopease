bdb9d6d228c039b702652814a78e08879cd150dc
cstuart756 <stujjc@yahoo.co.uk>
2025-12-05 09:32:48 +0000

    add shopease/urls.py

---
+++ shopease/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('apps.users.urls')),
+    path('products/', include('apps.products.urls')),
+    path('orders/', include('apps.orders.urls')),
+    path('', TemplateView.as_view(template_name='products/product_list.html'), name='home'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
